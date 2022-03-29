7e3777908e3353d346119fd19e42998460b8ea64
sriverar03 <srivera.r03@gmail.com>
2022-03-28 21:36:51 -0300

    Junit Test

--- src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.devops.dxc.devops;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DevopsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void getDxcTest() {
+		Assertions.assertEquals(600000, Util.getDxc(600000));
+		Assertions.assertEquals(1000000, Util.getDxc(1000000));
+		Assertions.assertEquals(1000000, Util.getDxc(3000000));
+		Assertions.assertEquals(1000000, Util.getDxc(9000000));
+		Assertions.assertEquals(1200000, Util.getDxc(12000000));
+		Assertions.assertEquals(4757700, Util.getDxc(50000000));
+	}
+
+	@Test
+	void getImpuestoTest() {
+		Assertions.assertEquals(8000, Util.getImpuesto(1600000, 100000));
+		Assertions.assertEquals(0, Util.getImpuesto(1400000, 100000));
+		Assertions.assertEquals(0, Util.getImpuesto(600000, 100000));		
+	}
+
+	@Test
+	void getSaldoRestanteTest() {
+		Assertions.assertEquals(4000000, Util.getSaldoRestante(5000000));
+		Assertions.assertEquals(33300000, Util.getSaldoRestante(37000000));
+	}
+
+	@Test
+	void getUfTest() {
+		Assertions.assertEquals(31718, Util.getUf());
 	}
 
 }
